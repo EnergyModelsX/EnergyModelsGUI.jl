99af1763bb76a9d8abba6039650e16c516e9dbce
Magnus Askeland <Magnus.Askeland@sintef.no>
2023-07-24 10:33:28 +0200

    Prototype implementation

--- ModelingToolkit/prototype.jl
+++ ModelingToolkit/prototype.jl
@@ -1,3 +1,8 @@
-using ModelingToolkit
-using ModelingToolkitDesigner
+#using ModelingToolkit
+#using ModelingToolkitDesigner
 using GLMakie
+using CairoMakie
+#using ModelingToolkit
+using FilterHelpers
+using FileIO
+using TOML
---
+++ ModelingToolkit/run_prototype.jl
@@ -0,0 +1,5 @@
+
+#Do the following to test this script:
+#1: generate a CE model that is loaded into memory.
+#2: Run the "prototype.jl" script.
+
