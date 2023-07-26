638c1a50289feea5888bab512752c0f5c264cd87
Magnus Askeland <Magnus.Askeland@sintef.no>
2023-07-26 09:41:59 +0200

    Updated script to automatically generate  CE model.

--- EnergySystemDesign/src/run_prototype.jl
+++ EnergySystemDesign/src/run_prototype.jl
@@ -1,14 +1,15 @@
-using DataFrames
-#To test this script:
-#1: generate a CE model that is loaded into memory.
-
-
+#To test this prototype package:
+#1: generate a CE model that is loaded into memory as "case".
+#include("generate_EMG.jl")
+include("generate_EMB.jl")
+#2: load the functions and data definitions:
 include("prototype.jl")
 
-
+#3: Specify path for design information:
 path = joinpath(@__DIR__, "design") # folder where visualization info is saved and retrieved
-design = EnergySystemDesign(case, path);
-#design = EnergySystemDesign
-view(design)
 
-DataFrame(name=[fieldnames(typeof(case[:nodes]))...], type=[fieldtypes(typeof(case[:nodes]))...])
+#4: Generate the system topology:
+design = EnergySystemDesign(case, path);
+
+#5: Plot the topology:
+view(design)
