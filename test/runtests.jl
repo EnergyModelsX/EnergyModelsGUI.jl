283746099c9e831aa6640d220b71a1b3987b69e5
Jon Vegard Venås <jonvegard.venas@sintef.no>
2023-12-13 08:42:53 +0100

    #22 Add to the CleanExport registry

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,8 @@ using EnergyModelsGUI
 using Test
 
 @testset "EnergyModelsGUI.jl" begin
-    # Write your tests here.
+    @test begin
+        x,y = EnergyModelsGUI.place_nodes_in_semicircle(7, 2, 1.1, 10.2, 60.06)
+        x ≈ 9.247372055837117 && y ≈ 60.61
+    end
 end
