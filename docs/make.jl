dcc765a4a4a0c14477b58ddfed11760b2197977d
Julian Straus <julian.straus@sintef.no>
2024-06-18 14:36:31 +0200

    Migration to GitHub from GitLab

--- docs/make.jl
+++ docs/make.jl
@@ -5,14 +5,12 @@ using EnergyModelsGUI
 const EMB = EnergyModelsBase
 const EMGUI = EnergyModelsGUI
 
-cd(dirname(@__FILE__)) # Make sure to be in the docs folder
-
 # Copy the NEWS.md file
-news = "src/manual/NEWS.md"
+news = "docs/src/manual/NEWS.md"
 if isfile(news)
     rm(news)
 end
-cp("../NEWS.md", news)
+cp("NEWS.md", news)
 
 DocMeta.setdocmeta!(
     EnergyModelsGUI, :DocTestSetup, :(using EnergyModelsGUI); recursive=true
@@ -20,13 +18,11 @@ DocMeta.setdocmeta!(
 
 makedocs(;
     sitename="EnergyModelsGUI.jl",
-    repo="https://gitlab.sintef.no/clean_export/energymodelsgui.jl/blob/{commit}{path}#{line}",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://clean_export.pages.sintef.no/energymodelsgui.jl/",
-        repolink="https://clean_export.pages.sintef.no/energymodelsgui.jl/",
         edit_link="main",
         assets=String[],
+        ansicolor=true,
     ),
     modules=[EnergyModelsGUI],
     pages=[
@@ -51,9 +47,6 @@ makedocs(;
     checkdocs=:all,
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(;
+    repo = "github.com/EnergyModelsX/EnergyModelsGUI.jl.git",
+)
