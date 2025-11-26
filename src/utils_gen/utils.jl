fedc32bfca57565b0316cf8d946b9c38f63a8e7b
Espen Flo Bødal <espen.bodal@sintef.no>
2025-11-26 10:49:23 +0100

    Add type_to_header for Int-index when reading results

--- src/utils_gen/utils.jl
+++ src/utils_gen/utils.jl
@@ -226,6 +226,7 @@ _type_to_header(::Type{<:TS.TimePeriod}) = :t
 _type_to_header(::Type{<:TS.TimeStructure}) = :t
 _type_to_header(::Type{<:Resource}) = :res
 _type_to_header(::Type{<:AbstractElement}) = :element
+_type_to_header(::Type{<:Int}) = :nr
 
 """
     save_results(model::Model; directory=joinpath(pwd(),"csv_files"))
